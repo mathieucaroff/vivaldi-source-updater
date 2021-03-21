c6ba0bd13b233a7f10353187ff9b56518760a28a
Mathieu CAROFF <mathieu.caroff@free.fr>
2021-03-21 22:31:43 +0100

    updater.py: listen on PORT

--- updater.py
+++ updater.py
@@ -1,1 +1,16 @@
-print("Updater hello world!!")
+from http import server
+import os
+import time
+import threading
+
+port = int(os.getenv("PORT"))
+
+threading.Thread(
+    target=server.HTTPServer(("", port), server.BaseHTTPRequestHandler).serve_forever,
+    daemon=True,
+).start()
+
+print(f"Listening on http://localhost:{port}")
+
+while True:
+    time.sleep(3600)
